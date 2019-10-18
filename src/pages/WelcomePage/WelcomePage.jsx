a8768f9098deef6e64f3ff72ecee49c10f55d757
Emily Landgrebe <emilandgrebe@gmail.com>
2019-10-18 16:27:23 -0500

    fix WelcomePage outline

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -5,13 +5,14 @@ class WelcomePage extends React.Component {
   render() {
     return (
       <div className='WelcomePage'>
-        <h1>DISCLAIMER: NOT ACTUALLY AFFILIATED 
+        <img className='bling' src='https://hannahmcook.files.wordpress.com/2018/07/7roc.gif?w=224&h=218'></img>
+        <h3>DISCLAIMER: NOT ACTUALLY AFFILIATED 
         WITH KENDRA SCOTT! JUST SELLING MY
-        OLD JEWELRY!</h1>
+        OLD JEWELRY!</h3>
         <h4>Great prices on quality jewelry that has been
           very lightly/never worn. Alot of these pieces are
           no longer sold in-store or they are custom pieces.
-          Prices are 50%-70% off in-store prices!If you have 
+          Prices are 50%-70% off in-store prices!If you have
           any questions, feel free to contact me!</h4>
       </div>
     )
